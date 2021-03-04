bf043f50c7835568d9e457bfa3645edd6e9a7583
SparshJain01 <76212908+SparshJain01@users.noreply.github.com>
2021-03-04 18:36:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function setup() {
+  createCanvas(800,400);
+
+ engine= Engine.create();
+	world = engine.world;
+
+  createSprite(400, 200, 50, 50);
+
+  Engine.run(engine);
+
+}
+
+function draw() {
+  background(255,255,255);
+  Engine.update(engine)
+  
+  drawSprites();
+
+}
